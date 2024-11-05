c3cadca4be37dba97fe69a0db57144b054f1355b
unknown <mmxmox@hotmail.com>
2024-11-05 12:29:50 +0300

    Not sure if theAnswer

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("It's working")
+const maxOfTwoNumbers =(x, y){
+  if (x >= y) => {
+    return x;
+  } else {
+    return y;
+  }
+}
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+const isAdult = (isAdult 21, Adult) {
+  if (isAdult(21) >= Adult) => {
+    return Adult;
+    else {
+      return 21;
+    }
+  }
+}
+
