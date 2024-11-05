c9da61ee2e2c2eb7adc28e96c2bea8b5730012f8
unknown <mmxmox@hotmail.com>
2024-11-05 22:30:58 +0300

    finish Ex 9 but 8 still not

--- app.js
+++ app.js
@@ -42,27 +42,27 @@
 
 // console.log('Exercise 6 Result:', maxOfThree(5, 10, 8))
 
-const calculateTip = (num1, num2) => {
-  return 50 * 0.2, 20 * 0.5
+// const calculateTip = (num1, num2) => {
+//   return 50 * 0.2, 20 * 0.5
+// }
+// console.log('Exercise 7 Result:', calculateTip(50, 20))
+
+// const convertTemperature = (Celsius, Fahrenheit) => {
+//   if (Celsius === 'C') {
+//     return Fahrenheit * (5 / 9) + 32
+//   } else if (Fahrenheit === 'F') {
+//     return Celsius * (5 / 9) - 32
+//   }
+// }
+
+// console.log('Exercise 8 Result:', convertTemperature(32, 'C'))
+
+const basicCalculator = (num1, num2, minus) => {
+  if (minus === 'subtract') {
+    return num1 - num2
+  }
 }
-console.log('Exercise 7 Result:', calculateTip(50, 20))
 
-// convertTemperature('C', 'F')
-// if (32, 'C') => {
-//   return 89.6;
-// } else if(32, 'F') => {
-//   return 0;
-// }
-// console.log('Exercise 8 Result:', convertTemperature(32,"C"));
-
-// basicCalculator('ass', 'subtract', 'multiply', 'divide')
-// if(10 - 5) => {
-//   return 5;
-// } else if(10 * 5) => {
-//   return 50;
-// } else if(10 / 5) => {
-//   return 2;
-// }
-// console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, 'subtract'))
 
 // calculateGrade(A, B, C, D, F)
